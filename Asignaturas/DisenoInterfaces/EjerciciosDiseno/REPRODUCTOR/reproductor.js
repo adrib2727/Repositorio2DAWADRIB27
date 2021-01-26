6538a168eadf81aa6e545f148c8572e8d3a0792f
adrib2727 <1464795@alu.murciaeduca.es>
2021-01-26 21:49:20 +0100

    26/01/21

--- Asignaturas/DisenoInterfaces/EjerciciosDiseno/REPRODUCTOR/reproductor.js
+++ Asignaturas/DisenoInterfaces/EjerciciosDiseno/REPRODUCTOR/reproductor.js
@@ -7,14 +7,26 @@ var varTiempo = document.getElementById("tiempo"); //Botón de tiempo de reprodu
 var siguiente = document.getElementById("bSiguiente"); //Botón de siguiente cancion.
 var anterior = document.getElementById("bAnterior"); //Botón de anterior cancion.
 var reproductor = document.getElementById("miAudio"); //Captura de la etiqueta audio.
+var h3 = document.getElementsByTagName("h3")[0];
+var lista = document.getElementsByTagName("ul")[0];
 
 /*Array que tiene las direcciones de los nombres de las
 canciones*/
 var arrayCan = ["canciones/Smooth Criminal.mp3", "canciones/Back In Black.mp3",
                 "canciones/Billie Jean.mp3", "canciones/Shot In The Dark.mp3", 
                 "canciones/Realize.mp3", "canciones/Toxicidad Fuera.mp3"];
+var arrayNombres = ["Smooth Criminal", "Back in Black", "Billie Jean", "Shoot in the Dark", "Realize",
+                    "Toxicidad Fuera"];
 var posicion = 0;
-reproductor.setAttribute("src", arrayCan[posicion]);
+var posNombre = 0;
+//Asigna la primera posicion en el array de direcciones.
+reproductor.setAttribute("src", arrayCan[posicion]); 
+//Escribe el título de la canción el reproductor.
+h3.innerHTML = arrayNombres[posNombre]; 
+
+/*Mostrar la lista de los nombres de reproducción.*/
+lista.innerHTML = "<li>"+arrayNombres.join("<li></li>")+"</li>";
+
 
 /* Reproducir audio */
 play.addEventListener("click", () => {
@@ -51,14 +63,26 @@ siguiente.addEventListener("click", () => {
     /*Si la posicion de la cancion es menor que la longitud del array, incrementar cada 
     vez que se haga el evento, */
     if(posicion < arrayCan.length - 1){
+        posNombre++;
         posicion++;
     }else if(posicion = arrayCan.length){
+        posNombre = 0;
         posicion = 0;
     }
     reproductor.setAttribute("src", arrayCan[posicion]); //Cambia la propiedad del atributo.
+    h3.innerHTML = arrayNombres[posNombre];
     reproductor.play(); //Reproduce la canción nada más cambiar.
 });
 
+
 anterior.addEventListener("click", () => {
-    
+    if(posicion != 0){
+        posNombre--;
+        posicion--;
+    }else{
+        posicion = arrayCan.length - 1;
+    }
+    reproductor.setAttribute("src", arrayCan[posicion]);
+    h3.innerHTML = arrayNombres[posNombre];
+    reproductor.play();
 });
