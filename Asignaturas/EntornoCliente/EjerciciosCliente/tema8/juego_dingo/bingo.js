aceb36ad80c16a0211f56b848afb9d15a700d6d8
adrib2727 <1464795@alu.murciaeduca.es>
2021-02-05 21:36:13 +0100

    05/02/21

--- Asignaturas/EntornoCliente/EjerciciosCliente/tema8/juego_dingo/bingo.js
+++ Asignaturas/EntornoCliente/EjerciciosCliente/tema8/juego_dingo/bingo.js
@@ -1,28 +1,61 @@
-let Palabras = ["jamones", "perro", "gato"];
-
-let secreta = Palabras[Math.floor(Math.random()* Palabras.length)];
+let Palabras = ["jamon", "perro", "gatos", "calles", "arbol"];
+let secreta = Palabras[Math.floor(Math.random() * Palabras.length)];
 let palabra = document.getElementsByTagName("input")[0];
 let enviar = document.getElementsByTagName("button")[0];
-let info = document.getElementsByTagName("h2")[0];
+let h2_mensaje = document.getElementsByTagName("h2")[0];
 let expr1 = /^[a-z]{5}$/;
-
-/*Evento de click*/
+let intentos = 0;
+console.log(secreta);
 enviar.addEventListener("click", () => {
-    /*Si la expresión regular no coincide con la palabra...*/
-    if(!expr1.test(palabra.value)){
-        info.innerHTML = "Tamaño incorrecto ";
-    }else{ //Tamaño correcto.
-        info.innerHTML = "Tamaño correcto";
-        //Bucle que recorre el input de la palabra.
-        let nuevaFila = document.createElement("div"); //Crea un div.
-        document.body.appendChild(nuevaFila); //Mete el div en el body.
-        nuevaFila.classList.add("fila"); //Le añade una clase llamada fila.
-
-        for(letra of palabra.value){
-            let nuevoDiv = document.createElement("div"); //Crea un div 
-            nuevoDiv.innerHTML = letra; //
-            nuevoDiv.classList.add("cuadrado");
-            nuevaFila.appendChild(nuevoDiv);
-        }
+    /*Si la palabra introducida es distinta que la secreta y los intentos son 
+    menores que 5 ==> Ir pintando las palabras introducidas.*/
+    if(palabra.value != secreta && intentos < 5){
+        pintarEstadoJuego("Juego comenzado");
+        pintarCuadros();
+        intentos++;
+        pintarIntentosJuego(intentos);
+    }else if(intentos == 5){ //Si los intentos llegan a 5, se acaban los intentos.
+        pintarEstadoJuego("Los intentos han sido agotados");
+    }else if(palabra.value == secreta){ //Si se acierta la secreta, Felicidades.
+        pintarEstadoJuego("¡Felicidades, has ganado!");
     }
 });
+
+/*FUNCIONES*/
+/*Función que pinta los cuadros*/
+function pintarCuadros(){
+    if(!expr1.test(palabra.value)){ //Tamaño incorrecto de la expresión regular.
+        h2_mensaje.innerHTML = "Tamaño incorrecto ";
+    }else{ //Tamaño correcto de la expresión regular.
+        h2_mensaje.innerHTML = "Tamaño correcto";
+
+        /*Crea una nueva fila (En el documento es un DIV).*/
+        let nuevaFila = document.createElement("div");
+        /*Selecciona el contenedor principal y le añade dentro nuevas filas.*/
+        document.getElementById("contenedor_principal").appendChild(nuevaFila);
+        /*Añade a los DIV la clase fila*/
+        nuevaFila.classList.add("fila");
+
+        for(letra of palabra.value){
+            let cuadrado = document.createElement("div"); //Crea un div 
+            cuadrado.innerHTML = letra;
+            cuadrado.classList.add("cuadrado");
+            nuevaFila.appendChild(cuadrado);
+        }
+    }
+}
+/*Función que maneja los intentos*/
+function pintarIntentosJuego(parametroIntento){
+    let intentoActual = document.getElementById("intentos_juego");
+    intentoActual.innerHTML = parametroIntento;
+    return intentoActual;
+}
+/*Función que escribe el estado del juego en el HTML*/
+function pintarEstadoJuego(parametroEstado){
+    let estadoActual = document.getElementById("estado_juego");
+    estadoActual.innerHTML = parametroEstado;
+    return estadoActual;
+}
+
+/*Convertir la palabra del array en un string y despues hacerle un bucle 
+que saque las letra una a una.*/
